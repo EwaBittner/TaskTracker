8cd8470be4212af2c6c9e4132605afad0ce75908
Ewa Bittner <bittner.ewa@gmail.com>
2022-02-21 19:22:16 +0100

    Finished task traker with static elements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from './components/AddTask';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: "Doctors Appointment",
-        day: '23.02.2022 at 2pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: "Meeting at Uni",
-        day: '13.03.2022 at 10am',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: "Food shopping",
-        day: '20.03.2022 at 3:30pm',
-        reminder: false
-    },
-])
+      id: 1,
+      text: "Doctors Appointment",
+      day: "23.02.2022 at 2pm",
+      reminder: true
+  },
+  {
+      id: 2,
+      text: "Meeting at Uni",
+      day: "13.03.2022 at 10am",
+      reminder: true
+  },
+  {
+      id: 3,
+      text: "Food shopping",
+      day: "20.03.2022 at 3:30pm",
+      reminder: false
+  }
+  ])
+
+//   useEffect(() => {
+//     const getTasks = async () => {
+//       const tasksFromServer = await fetchTasks()
+//       setTasks(tasksFromServer)
+//     }
+
+//     getTasks()
+//   }, [])
+
+// // Fetch Tasks
+// const fetchTasks = async () => {
+//   const res = await fetch('https://localhost:5000/tasks')
+//   const data = await res.json()
+
+//   return data
+// }
+
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
 
 // Delete task
 const deleteTask = (id) => {
@@ -42,8 +68,11 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header 
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
         <Tasks 
           tasks={tasks} 
--- my-app/src/components/AddTask.js
+++ my-app/src/components/AddTask.js
@@ -1,12 +1,27 @@
 import { useState } from 'react';
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert('Please add a task')
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
         <div className="form-control">
             <label>Task</label>
             <input type='text' 
@@ -25,6 +40,7 @@ const AddTask = () => {
             <label>Set Reminder</label>
             <input 
                 type='checkbox' 
+                checked={reminder}
                 value={reminder} 
                 onChange={(e) => setReminder(e.currentTarget.checked)}></input>
         </div>
